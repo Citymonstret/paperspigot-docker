e523e4e548a4247d7fc654b7e2d533fe8e6c3c1f
Sauilitired <sauilitired@gmail.com>
2019-09-10 16:11:23 +0200

    Move -jar

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -12,11 +12,11 @@ if [[ "$1" = 'serve' ]];  then
   echo "Starting the server with ${JAVA_HEAP_SIZE}M allocated heap!"
 
   # Start server
-  java -jar $JAVA_ARGS \	  
+  java -$JAVA_ARGS \	  
     -Xmx${JAVA_HEAP_SIZE}M -Xms${JAVA_HEAP_SIZE}M \
     -Xmns${NURSERY_MINIMUM}M -Xmnx${NURSERY_MAXIMUM}M \
     -Xgc:concurrentScavenge -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:scvNoAdaptiveTenure -Xdisableexplicitgc \
-    $SERVER_PATH/paper.jar \
+    -jar $SERVER_PATH/paper.jar \
     $SPIGOT_ARGS \
     --bukkit-settings $CONFIG_PATH/bukkit.yml --plugins $PLUGINS_PATH --world-dir $WORLDS_PATH --spigot-settings $CONFIG_PATH/spigot.yml --commands-settings $CONFIG_PATH/commands.yml --config $PROPERTIES_LOCATION \
     --paper-settings $CONFIG_PATH/paper.yml \
